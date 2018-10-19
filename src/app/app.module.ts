066d2460f828485a968ae932407b945900eeee84
aaltair93 <a.altair@ns4it.com>
2018-10-19 00:34:40 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModalModule}  from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule}  from '@ng-bootstrap/ng-bootstrap';
+
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+ import {MatButtonModule, MatCheckboxModule  } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent ,
+    HomeComponent ,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule ,
+    NgbModalModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+     MatCheckboxModule 
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
